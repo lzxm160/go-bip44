4c62ab222598dc3c6bceea6515d59238f6dbd930
limingming <limingming@wident.cn>
2017-11-17 15:57:11 +0800

    xx

--- rungo.sh
+++ rungo.sh
@@ -2,8 +2,8 @@ export GOROOT=/usr/local/go
 export GOPATH=/root/gopath
 export GOBIN=/root/gopath/bin
 
-#rm -fr hdwallet
-#go build -o hdwallet ./
-#./hdwallet
+rm -fr hdwallet
+go build -o hdwallet ./
+./hdwallet
 # go run gowallet.go -n 3 -v BTC
-go run gowallet.go
+# go run gowallet.go
