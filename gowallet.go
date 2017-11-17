17c2a5fa49bad9b7e95b886f92409b4303629e8e
limingming <limingming@wident.cn>
2017-11-17 16:05:34 +0800

    xx

--- gowallet.go
+++ gowallet.go
@@ -12,50 +12,52 @@ func main() {
 	testNewKeyFromMasterKey()
 }
 func testNewKeyFromMnemonic() {
-	mnemonic := "yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow"
+	mnemonic := "cannon ranch answer cherry chunk market muscle mesh rail rice wheel wrong"
 	fKey, err := NewKeyFromMnemonic(mnemonic, TypeFactomFactoids, bip32.FirstHardenedChild, 0, 0)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	if fKey.String() != "xprvA2vH8KdcBBKhMxhENJpJdbwiU5cUXSkaHR7QVTpBmusgYMR8NsZ4BFTNyRLUiaPHg7UYP8u92FJkSEAmmgu3PDQCoY7gBsdvpB7msWGCpXG" {
-		fmt.Printf("Invalid Factoid key - %v", fKey.String())
-	}
-
+	// if fKey.String() != "xprvA2vH8KdcBBKhMxhENJpJdbwiU5cUXSkaHR7QVTpBmusgYMR8NsZ4BFTNyRLUiaPHg7UYP8u92FJkSEAmmgu3PDQCoY7gBsdvpB7msWGCpXG" {
+	// 	fmt.Printf("Invalid Factoid key - %v", fKey.String())
+	// }
+	fmt.Println(fKey.String())
 	ecKey, err := NewKeyFromMnemonic(mnemonic, TypeFactomEntryCredits, bip32.FirstHardenedChild, 0, 0)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	if ecKey.String() != "xprvA2ziNegvZRfAAUtDsjeS9LvCP1TFXfR3hUzMcJw7oYAhdPqZyiJTMf1ByyLRxvQmGvgbPcG6Q569m26ixWsmgTR3d3PwicrG7hGD7C7seJA" {
-		fmt.Printf("Invalid EC key - %v", ecKey.String())
-	}
+	// if ecKey.String() != "xprvA2ziNegvZRfAAUtDsjeS9LvCP1TFXfR3hUzMcJw7oYAhdPqZyiJTMf1ByyLRxvQmGvgbPcG6Q569m26ixWsmgTR3d3PwicrG7hGD7C7seJA" {
+	// 	fmt.Printf("Invalid EC key - %v", ecKey.String())
+	// }
+	fmt.Println(ecKey.String())
 }
 
 func testNewKeyFromMasterKey() {
-	mnemonic := "yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow"
+	mnemonic := "cannon ranch answer cherry chunk market muscle mesh rail rice wheel wrong"
 
 	seed, err := bip39.NewSeedWithErrorChecking(mnemonic, "")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 
 	masterKey, err := bip32.NewMasterKey(seed)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-
+	fmt.Println(masterKey)
 	fKey, err := NewKeyFromMasterKey(masterKey, TypeFactomFactoids, bip32.FirstHardenedChild, 0, 0)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	if fKey.String() != "xprvA2vH8KdcBBKhMxhENJpJdbwiU5cUXSkaHR7QVTpBmusgYMR8NsZ4BFTNyRLUiaPHg7UYP8u92FJkSEAmmgu3PDQCoY7gBsdvpB7msWGCpXG" {
-		fmt.Printf("Invalid Factoid key - %v", fKey.String())
-	}
-
+	// if fKey.String() != "xprvA2vH8KdcBBKhMxhENJpJdbwiU5cUXSkaHR7QVTpBmusgYMR8NsZ4BFTNyRLUiaPHg7UYP8u92FJkSEAmmgu3PDQCoY7gBsdvpB7msWGCpXG" {
+	// 	fmt.Printf("Invalid Factoid key - %v", fKey.String())
+	// }
+	fmt.Println(fKey.String())
 	ecKey, err := NewKeyFromMasterKey(masterKey, TypeFactomEntryCredits, bip32.FirstHardenedChild, 0, 0)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	if ecKey.String() != "xprvA2ziNegvZRfAAUtDsjeS9LvCP1TFXfR3hUzMcJw7oYAhdPqZyiJTMf1ByyLRxvQmGvgbPcG6Q569m26ixWsmgTR3d3PwicrG7hGD7C7seJA" {
-		fmt.Printf("Invalid EC key - %v", ecKey.String())
-	}
+	// if ecKey.String() != "xprvA2ziNegvZRfAAUtDsjeS9LvCP1TFXfR3hUzMcJw7oYAhdPqZyiJTMf1ByyLRxvQmGvgbPcG6Q569m26ixWsmgTR3d3PwicrG7hGD7C7seJA" {
+	// 	fmt.Printf("Invalid EC key - %v", ecKey.String())
+	// }
+	fmt.Println(ecKey.String())
 }
